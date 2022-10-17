9536331390c13657184f0768520dbae538cd542b
Volodymyr Sokolov <sokolmx@gmail.com>
2022-10-17 14:38:47 +0300

    add-constructor

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,33 +1,48 @@
-function Carousel () {
-
+function Carousel (containerID = '#carousel', slideID = '.slide', interval = 2000) {
   this.container = document.querySelector('#carousel');
   this.slides = this.container.querySelectorAll('.slide');
-  this.indicatorsContainer = this.container.querySelector('#indicators-container');
-  this.indicators = this.indicatorsContainer.querySelectorAll('.indicator');
-  this.prevBtn = this.container.querySelector('#prev');
-  this.pauseBtn = this.container.querySelector('#pause');
-  this.nextBtn = this.container.querySelector('#next');
-  
-  this.SLIDES_COUNT = this.slides.length; 
-  this.CODE_LEFT_ARROW = 'ArrowLeft';
-  this.CODE_RIGHT_ARROW = 'ArrowRight';
-  this.CODE_SPACE = 'Space';
-
-  this.interval = 2000;
-  this.isPlaying = true;
-  this.currentSlide = 0;
+  this.interval = interval;
 }
 
 Carousel.prototype = {
-  pausePlay() {
-    if (this.isPlaying) this._pause();
-    else this._play();
+  _initProps() {
+    this.SLIDES_COUNT = this.slides.length; 
+    this.CODE_LEFT_ARROW = 'ArrowLeft';
+    this.CODE_RIGHT_ARROW = 'ArrowRight';
+    this.CODE_SPACE = 'Space';
+
+    this.isPlaying = true;
+    this.currentSlide = 0;
   },
 
-  init() {
-    this.timerID = setInterval(this._gotoNext.bind(this), this.interval);
-    this._initListeners();
+  _initControls() {
+    const controls = document.createElement('div');
+    const PAUSE = '<span class="control" id="pause"><i class="fa-regular fa-circle-pause"></i></span>';
+    const PREV = '<span class="control" id="prev"><i class="fa-solid fa-angle-left"></i></span>';
+    const NEXT = '<span class="control" id="next"><i class="fa-solid fa-angle-right"></i></span>';
+    controls.setAttribute('class','controls');
+    this.container.append(controls);
+    controls.innerHTML = PREV + PAUSE + NEXT;
+    this.prevBtn = this.container.querySelector('#prev');
+    this.pauseBtn = this.container.querySelector('#pause');
+    this.nextBtn = this.container.querySelector('#next');
   },
+
+  _initIndicators() {
+    const indicators = document.createElement('div');
+    indicators.setAttribute('class','indicators');
+
+    for (let i = 0; i < this.SLIDES_COUNT; i++) {
+      const indItem = document.createElement('div');
+      indItem.setAttribute('class', i !== 0 ? 'indicator' : 'indicator active');
+      indItem.dataset.slideTo = `${i}`;
+      indicators.append(indItem);    
+    }
+    this.container.append(indicators);
+
+    this.indContainer = this.container.querySelector('.indicators');
+    this.indicators = this.indContainer.querySelectorAll('.indicator');
+   },
 
   _gotoNth(n){
     this.slides[this.currentSlide].classList.toggle('active');
@@ -52,7 +67,7 @@ Carousel.prototype = {
   },
   
   _play() {
-    this.timerID = setInterval(this._gotoNext.bind(this), this.interval);
+    this._tick();
     this.isPlaying = true;
     this.pauseBtn.innerHTML = '<i class="fa-regular fa-circle-pause"></i>';
   },
@@ -88,7 +103,24 @@ Carousel.prototype = {
     this.pauseBtn.addEventListener('click', this.pausePlay.bind(this));
     this.nextBtn.addEventListener('click', this._next.bind(this));
     this.prevBtn.addEventListener('click', this._prev.bind(this));
-    this.indicatorsContainer.addEventListener('click', this._indicate.bind(this));
+    this.indContainer.addEventListener('click', this._indicate.bind(this));
+  },
+
+  _tick() {
+    this.timerID = setInterval(this._gotoNext.bind(this), this.interval);
+  },
+
+  pausePlay() {
+    if (this.isPlaying) this._pause();
+    else this._play();
+  },
+
+  init() {
+    this._initProps();
+    this._initIndicators();
+    this._initControls();
+    this._initListeners();
+    this._tick();
   }
 };
 
